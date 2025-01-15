13f974cd3a07c57dac0ededb1f746640b6ae99fc
craftykate <katemcfaul@gmail.com>
2025-01-14 20:17:11 -0800

    Add icon to top row of dice

--- components/dice/SingleDice.tsx
+++ components/dice/SingleDice.tsx
@@ -1,10 +1,21 @@
-import { Paper, Group, Center } from "@mantine/core";
+import { Paper, Group, Center, ActionIcon } from "@mantine/core";
+import { IconSettings } from "@tabler/icons-react";
 import DiceFace from "./DiceFace";
 
 export default function SingleDice() {
   return (
     <Paper withBorder>
-      <Group>top row</Group>
+      <Group justify="flex-end">
+        <ActionIcon variant="white">
+          <IconSettings
+            stroke={1.5}
+            color="gray"
+            style={{
+              cursor: "pointer",
+            }}
+          />
+        </ActionIcon>
+      </Group>
       <Center>
         <DiceFace />
       </Center>
